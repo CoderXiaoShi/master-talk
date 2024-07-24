c4bd611a54ccd6f15b8bed2f74365c04b285ac3e
石兴龙 <1003865572@qq.com>
2024-07-24 09:05:59 +0800

    1

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import Head from './components/Head.vue'
+import NewsList from './components/NewsList.vue'
 </script>
 
 <template>
   <Head />
-  <div>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
-  </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <NewsList />
 </template>
 
-<style scoped>
-
+<style lang="less" scoped>
 </style>
---
+++ website/src/components/NewsList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="news">
+    <NewsItem />
+    <NewsItem />
+    <NewsItem />
+    <NewsItem />
+  </div>
+</template>
+<script setup>
+import NewsItem from './NewsItem.vue'
+
+</script>
+
+<style lang="less" scoped>
+.news{
+  padding: 5vw 0;
+}
+</style>
