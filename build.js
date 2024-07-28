c55c219ac0ee34e83c48883cb22b96d8c148b789
石兴龙 <1003865572@qq.com>
2024-07-28 15:26:46 +0800

    1

--- build.js
+++ build.js
@@ -1,4 +1,9 @@
-/*
-  1. 数据库 -> json
-  2. 编译前端代码 + json
-*/
+const { execSync } = require('child_process')
+
+
+execSync('git pull')
+
+// 每隔半个小时拉一次代码
+setTimeout(() => {
+  execSync('git pull')
+}, 1000 * 60 * 30)
