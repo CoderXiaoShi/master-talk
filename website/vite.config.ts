b675b55d5b854a180e9c95286134a886b7b48f39
石兴龙 <1003865572@qq.com>
2024-07-28 14:41:41 +0800

    2

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'home',
+  },
   resolve: {
     alias: [
       {
