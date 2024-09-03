3664911b45853933b2e440982bbc2da80122f511
石兴龙 <1003865572@qq.com>
2024-09-03 16:37:18 +0800

    update

--- data-collection/index.js
+++ data-collection/index.js
@@ -12,7 +12,7 @@ const browser = await puppeteer.launch({
   // headless: true,
   slowMo: 250,
   args: ['--window-size=1920,1080'],
-  userDataDir: './dist',
+  userDataDir: './browser-data',
 });
 
 const sleep = num => new Promise(resolve => setTimeout(resolve, num));
@@ -21,7 +21,7 @@ const getNews = async (originId) => {
 
   const page = await browser.newPage();
   await page.goto(`https://x.com/${originId}`);
-  await page.setViewport({ width: 1680, height: 1080 });
+  await page.setViewport({ width: 1280, height: 1080 });
 
   /*
     区分消息类型  
@@ -37,77 +37,81 @@ const getNews = async (originId) => {
   }
 
   page.on('response', async response => {
-    // UserByScreenName banner
 
-    if (response.url().includes('UserByScreenName')) {
-      let res = await response.json(); // 名称
-      const {
-        name, // 昵称
-        profile_banner_url, // banner
-        profile_image_url_https, // 头像
-        screen_name, // id
-      } = res.data.user.result.legacy;
-      let photo = profile_image_url_https.replace('_normal.jpg', '_200x200.jpg')
-      author.name = name;
-      author.photo = photo.split('/').pop();
-      author.profile_banner_url = profile_banner_url.split('/').pop();
-      author.screen_name = screen_name;
-      // console.log('profile_banner_url', profile_banner_url)
-      // console.log('photo', photo)
-      // 下载banner
-      await downloadUrl(browser, profile_banner_url);
-      // 下载头像
-      await downloadUrl(browser, photo);
-    }
-    // 用户必须登录, 否则会报错
-    if (response.url().includes('/UserTweets')) {
-      try {
-        let res = await response.json();
-
-        const { instructions } = res.data.user.result.timeline_v2.timeline
-        for (const item of instructions) {
-          if (item.type === 'TimelineAddEntries') {
-            const { entries } = item;
-
-            for (const articleItem of entries) {
-              /*
-                区分消息类型  
-                1. 纯文本
-                2. 纯文本 + 图片
-                3. 纯转帖 纯文本 + 图片
-              */
-              if (articleItem.content.entryType === 'TimelineTimelineItem') {
-                try {
-                  const rest_id = articleItem.content.itemContent.tweet_results.result.rest_id
-                  const article = await Article.findAll({ where: { rest_id } });
-                  if (article.length == 0) {
-                    await Article.create({
-                      jsonStr: JSON.stringify(articleItem.content),
-                      rest_id,
-                      author: JSON.stringify(author),
-                      originId: originId,
-                      content: articleItem.content.itemContent.tweet_results.result.legacy.full_text
-                    })
-                  }
-                } catch (error) {
-                  console.log(2, error)
-                }
-                const media = articleItem.content.itemContent.tweet_results?.result?.legacy?.entities?.media || []
-                if (Array.isArray(media)) {
-                  for (const item of media) {
-                    await downloadUrl(browser, item.media_url_https);
-                  }
-                }
-              } else if (articleItem.content.entryType === 'TimelineTimelineCursor') {
-                console.log('cursor')
-              }
-            }
-
-          }
-        }
-      } catch (error) {
-        console.log(1, error)
+    try {
+          // UserByScreenName banner
+      if (response.url().includes('UserByScreenName')) {
+        let res = await response.json(); // 名称
+        const {
+          name, // 昵称
+          profile_banner_url, // banner
+          profile_image_url_https, // 头像
+          screen_name, // id
+        } = res.data.user.result.legacy;
+        let photo = profile_image_url_https.replace('_normal.jpg', '_200x200.jpg')
+        author.name = name;
+        author.photo = photo.split('/').pop();
+        author.profile_banner_url = profile_banner_url.split('/').pop();
+        author.screen_name = screen_name;
+        // console.log('profile_banner_url', profile_banner_url)
+        // console.log('photo', photo)
+        // 下载banner
+        await downloadUrl(browser, profile_banner_url);
+        // 下载头像
+        await downloadUrl(browser, photo);
       }
+      // 用户必须登录, 否则会报错
+      if (response.url().includes('/UserTweets')) {
+        try {
+          let res = await response.json();
+
+          const { instructions } = res.data.user.result.timeline_v2.timeline
+          for (const item of instructions) {
+            if (item.type === 'TimelineAddEntries') {
+              const { entries } = item;
+
+              for (const articleItem of entries) {
+                /*
+                  区分消息类型  
+                  1. 纯文本
+                  2. 纯文本 + 图片
+                  3. 纯转帖 纯文本 + 图片
+                */
+                if (articleItem.content.entryType === 'TimelineTimelineItem') {
+                  try {
+                    const rest_id = articleItem.content.itemContent.tweet_results.result.rest_id
+                    const article = await Article.findAll({ where: { rest_id } });
+                    if (article.length == 0) {
+                      await Article.create({
+                        jsonStr: JSON.stringify(articleItem.content),
+                        rest_id,
+                        author: JSON.stringify(author),
+                        originId: originId,
+                        content: articleItem.content.itemContent.tweet_results.result.legacy.full_text
+                      })
+                    }
+                  } catch (error) {
+                    console.log(2, error)
+                  }
+                  const media = articleItem.content.itemContent.tweet_results?.result?.legacy?.entities?.media || []
+                  if (Array.isArray(media)) {
+                    for (const item of media) {
+                      await downloadUrl(browser, item.media_url_https);
+                    }
+                  }
+                } else if (articleItem.content.entryType === 'TimelineTimelineCursor') {
+                  console.log('cursor')
+                }
+              }
+
+            }
+          }
+        } catch (error) {
+          console.log(1, error)
+        }
+      }
+    } catch (error) {
+      console.error(error)
     }
   })
   return page
